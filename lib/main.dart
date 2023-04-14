3d50632fbbe01d64c12777dd18d6f6f51f5edf23
Nikshit Chauhan <nikshit2012@gmail.com>
2023-04-15 02:15:34 +0530

    feat: added easyLoading for banner uploading

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,21 @@
+import 'dart:io';
 import 'package:e_commerce_admin/views/screens/main_screen.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+      options: kIsWeb || Platform.isAndroid
+          ? FirebaseOptions(
+              apiKey: "AIzaSyAXEpFq6x8tT6vStJs498tE6kGfZSydrns",
+              appId: "1:920932779651:web:6f69298e48f70ca9fc7db6",
+              messagingSenderId: "920932779651",
+              projectId: "testinga-ee291",
+              storageBucket: "testinga-ee291.appspot.com")
+          : null);
   runApp(const MyApp());
 }
 
@@ -24,6 +32,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.red,
       ),
       home: const MainScreen(),
+      builder: EasyLoading.init(),
     );
   }
 }
--- lib/views/screens/side_bar_screens/upload_banner_screen.dart
+++ lib/views/screens/side_bar_screens/upload_banner_screen.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
 
 class UploadBannerScreen extends StatefulWidget {
   static const String routeName = '\UploadBannerScreen';
@@ -42,11 +43,14 @@ class _UploadBannerScreenState extends State<UploadBannerScreen> {
   }
 
   _uploadToFireStore() async {
+    EasyLoading.show();
     if (_image != null) {
       String imageUrl = await _uploadBannersToStorage(_image);
 
       await _firestore.collection('banners').doc(fileName).set({
         'image': imageUrl,
+      }).whenComplete(() {
+        EasyLoading.dismiss();
       });
     }
   }
