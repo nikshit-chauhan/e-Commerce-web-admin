fb30876dd2537dc2c1d14810338511b4b60cf24f
Nikshit Chauhan <nikshit2012@gmail.com>
2023-06-16 17:16:39 +0530

    feat: added category screen

--- lib/views/screens/side_bar_screens/category_screen.dart
+++ lib/views/screens/side_bar_screens/category_screen.dart
@@ -6,16 +6,71 @@ class CategoryScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-      child: Container(
-        alignment: Alignment.topLeft,
-        padding: const EdgeInsets.all(10),
-        child: const Text(
-          'Categories',
-          style: TextStyle(
-            fontWeight: FontWeight.w700,
-            fontSize: 36,
+      child: Column(
+        children: [
+          Container(
+            alignment: Alignment.topLeft,
+            padding: const EdgeInsets.all(10),
+            child: const Text(
+              'Category',
+              style: TextStyle(
+                fontWeight: FontWeight.w700,
+                fontSize: 36,
+              ),
+            ),
           ),
-        ),
+          Divider(
+            color: Colors.grey,
+          ),
+          Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Column(
+                  children: [
+                    Container(
+                      height: 140,
+                      width: 140,
+                      decoration: BoxDecoration(
+                        color: Colors.grey.shade300,
+                        border: Border.all(color: Colors.grey.shade800),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Center(
+                        child: Text('Category'),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 12,
+                    ),
+                    ElevatedButton(
+                      onPressed: () {},
+                      child: Text('Upload Image'),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Flexible(
+                  child: SizedBox(
+                    width: 200,
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        labelText: 'Enter Category Name',
+                        hintText: 'Enter Category Name',
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text('Save'),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
